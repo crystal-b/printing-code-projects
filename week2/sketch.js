a7199f2fc5d7433891c7e50a56039d06576afb35
crystal-b <git@github.com:crystal-b>
2015-10-01 15:07:03 -0400

    3d satellite

--- week2/sketch.js
+++ week2/sketch.js
@@ -27,13 +27,15 @@ function triangle(x1, y1, x2, y2, x3, y3, c, s, sw) {
 
 function satellite() {
   //solar panels
-  rectangle(200, 200, 200, 100, 0, false, 0)
-  rectangle(600, 200, 200, 100, 0, false, 0)
+  rectangle(340, -40, 200, 100, 0, false, 45)
+  rectangle(640, 275, 200, 100, 0, false, 45)
   //satellite
   ellipse(500, 250, 150, 150, 0, false, 0)
+  ellipse(550, 200, 150, 150, 0, false, 0)
+  rectangle(500, 145, 50, 80, 0, false, 45)
+  rectangle(575, 210, 50, 80, 0, false, 45)
   //connecting lines
-  rectangle(400, 245, 50, 10, 0, false, 0)
-  rectangle(550, 245, 50, 10, 0, false, 0)
+  rectangle(440, 135, 250, 10, 0, false, 45)
 }
 
 satellite()
