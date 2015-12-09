1b23e2817a917e412dc94ae0fe9b97a7574c984d
crystal-b <git@github.com:crystal-b>
2015-12-09 13:17:23 -0500

    testing out adding noise to bkg shapes

--- final/sketch.js
+++ final/sketch.js
@@ -8,9 +8,7 @@ var r = new Rune({
 });
 
 
-function nothing() {
-
-//VARIABLES
+/* * * VARIABLES * * */
   var x = 0;
   var y = 0;
   var w = 50;
@@ -19,42 +17,76 @@ function nothing() {
   var width = 850;
   var s = 50;
   var z = 100;
-  var dynamic = true;
+  var dynamic = false;
 
-//SHAPES
+
+/* * * SHAPES * * */
   function rectangle() {
     r.rect(x, y, w, h)
       .fill(0)
       .stroke(false);
   }
+  // function rectanglePath() {
+  //   var rectPath = r.path(x, y)
+  //     .lineTo(w, 0)
+  //     .lineTo(w, h)
+  //     .lineTo(0, h)
+  //     .closePath()
+  //     .fill(0)
+  //     .stroke(false);
+  //   rectPath.move(x, y);
+  // }
+
   function rectanglePath() {
-    // var rectPath = r.path(x, y)
-    //   .lineTo(w, y)
-    //   .lineTo(w, h)
-    //   .lineTo(x, h)
-    //   .closePath()
-    //   .fill(0)
-    //   .stroke(false);
-    // var rectPath = r.path(0, 0)
-    //   .lineTo(50, 0)
-    //   .lineTo(50, 30)
-    //   .lineTo(0, 30)
-    //   .closePath()
-    //   .fill(0)
-    //   .stroke(false);
-    var rectPath = r.path(x, y)
-      .lineTo(w, 0)
-      .lineTo(w, h)
-      .lineTo(0, h)
-      .closePath()
+    var xStep = 15;
+    var yStep = 5;
+    var noise = new Rune.Noise();
+    var noiseStep = 0;
+    var noiseStep2 = 50;
+    var noiseStep3 = 30;
+    var rectPath = r.path(0, 0)
       .fill(0)
-      .stroke(false);
+      .stroke(0);
+      for(var j = 0; j <= 50; j += xStep) {
+        var k = noise.get(noiseStep) * 5;
+        rectPath.lineTo(j, k);
+
+        noiseStep += 0.1;
+      }
+      // for(k = 0; k <= 30; k += yStep) {
+      //   j = noise.get(noiseStep2) * 100;
+      //   rectPath.lineTo(j, k);
+
+      //   noiseStep2 += 0.01;
+      //   console.log(j);
+      // }
+      for(j = 50; j <= 50 && j >= 0; j -= xStep) {
+        k = noise.get(noiseStep3) * 60;
+        rectPath.lineTo(j, k);
+
+        noiseStep3 += 0.1;
+      }
+      // for(k = 30; k <= 30; k -= yStep) {
+      //   j = noise.get(noiseStep) * 5;
+      //   rectPath.lineTo(j, k);
+
+      //   noiseStep += 0.1;
+      // }
 
     rectPath.move(x, y);
   }
+  /* * * NOISE * * */
+// var xStep = 15;
+// var noise = new Rune.Noise();
+// var noiseStep = 0;
+// for(var j = 0; j <= w; j += xStep) {
+//   var k = noise.get(noiseStep) * 100;
+//   rectanglePath.lineTo(j, k);
+
+//   noiseStep += 0.1;
+// }
+
   function bullet() {
-    //what the hell is this doing?
-    //var bulletGroup = r.group(x, y, parent);
     var bulletGroup = r.path(50, 50)
       .lineTo(10, 0)
       .lineTo(10, 20)
@@ -68,7 +100,6 @@ function nothing() {
       .closePath()
       .fill(255, 0, 0)
       .stroke(false);
-
     bulletGroup.move(x, y);
   }
   function no() {
@@ -76,7 +107,8 @@ function nothing() {
     var sc = 255;
     var sw = 70;
     r.circle(width/2, height/2, diameter)
-      .fill(false)
+      // .fill(false)
+      .fill(255)
       .stroke(sc)
       .strokeWidth(sw);
     r.rect(400, 300, sw, diameter*2)
@@ -85,34 +117,49 @@ function nothing() {
       .rotate(-45, width/2, height/2);
   }
 
-//ALGORITHM
+
+
+
+/* * * ALGORITHMS * * */
+function emphasize() {
+
+}
+function nothing() {
 // set row Y
   for (y = 0; y <= height; y += s) {
-// set column X
+  // set column X
     for (x = 0; x <= width; x += z) {
       if (dynamic === true) {
       // set column width
         for (i = 0; i <= 5; i++) {
-          w=50;
+        // set min/max column widths
+          w = 50;
           w = w * Math.round(Rune.random(1,3));
-      // call dynamic positive shapes
+        // call dynamic positive shapes
           rectanglePath();
           x = x+20;
         }
       }
       else {
-      // call positive shapes
-        bullet();
+      // adjust column width
+        z = 72;
+      // call non-dynamic positive shapes
+        //bullet();
+        rectanglePath();
       }
     }
   }
 // call negative shapes
-  //rectanglePath();
   no();
 }
 
+// if (array[j].x >= object[*].x && array[j].y >= object[*].y) {
+//   move x 5 pixels away
+//   move y 5 pixels away
+// }
 
-//MAKE STUFF HAPPEN
+
+/* * * MAKE STUFF HAPPEN * * */
 nothing();
 
 r.draw();
