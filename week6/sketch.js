1439744fbf66d26b7769f42146942be3802ba98b
crystal-b <git@github.com:crystal-b>
2016-01-07 21:10:21 -0500

    adding updates from winter show poster

--- week6/sketch.js
+++ week6/sketch.js
@@ -3,9 +3,12 @@
 var r = new Rune({
   container: "#canvas",
   width: 1700,
-  height: 1900,
+  height: 2000,
   //debug: true
 });
+
+var rectY1 = 950;
+var rectY2 = 1000;
 
 /***   GRID   ***/
 var grid1 = r.grid({
@@ -44,32 +47,32 @@ var grid3 = r.grid({
 console.log(grid3.vars);
 
 /***   TEXT   ***/
-r.text("THE", 1170, 200)
-  .fill(30)
+r.text("THE", 1120, 100)
+  .fill(40)
   .stroke(false)
   .fontSize(220)
   //.textAlign("center")
   .fontFamily("Futura")
   .rotate(180, 1375, 100)
 
-r.text("Satanic", 1800, 150)
-  .fill(30)
+r.text("Satanic", 1900, 100)
+  .fill(40)
   .stroke(false)
   .fontSize(500)
   //.textAlign("center")
   .fontFamily("Futura")
   .rotate(90, 1500, -150)
 
-r.text("V", 1650, 645)
-  .fill(30)
+r.text("V", 1750, 595)
+  .fill(40)
   .stroke(false)
   .fontSize(640)
   //.textAlign("center")
   .fontFamily("Futura")
   .rotate(90, 1500, -150)
 
-r.text("erses", 2060, 460)
-  .fill(30)
+r.text("erses", 2160, 410)
+  .fill(40)
   .stroke(false)
   .fontSize(612)
   //.textAlign("center")
@@ -81,7 +84,7 @@ r.text("erses", 2060, 460)
 /***   SHAPES   ***/
 function triangle(x1, y1, x2, y2, x3, y3)  {
   r.triangle(x1, y1, x2, y2, x3, y3)
-    .fill('hsv', 270, 90.5, 32.9)
+    .fill('hsv', 270, 90.5, 32.9, .8)
     .stroke(false)
 }
 
@@ -92,10 +95,10 @@ function rectangle(x, y, w, h, hue, sat, val, rot) {
     .rotate(rot, x, y)
 }
 
-triangle(1600, 900, 1100, 1100, 1100, 700)
+triangle(1650, 1000, 1150, 1200, 1150, 800)
 
-r.text("erses", 2060, 460)
-  .fill(30)
+r.text("erses", 2160, 410)
+  .fill(40)
   .stroke(false)
   .fontSize(612)
   //.textAlign("center")
@@ -103,44 +106,47 @@ r.text("erses", 2060, 460)
   .rotate(90, 1500, -150)
 
 for (i=1100; i>0; i-=200) {
-  rectangle(i, 850, 100, 50, 16, 68.6, 100, 15)
+  rectangle(i, rectY1, 100, 50, 5, 99.6, 99.8, 15);
+  rectY1 -= 10;
 }
-for (i=1100; i>0; i-=200) {
-  rectangle(i, 900, 100, 50, 5, 99.6, 99.8, -15)
+for (i=1050; i>0; i-=200) {
+  rectangle(i, rectY2, 100, 50, 16, 68.6, 100, -15);
+  rectY2 += 10;
 }
 
-for (j=670; j<800; j+=200) {
+
+for (j=820; j<950; j+=200) {
   rectangle(900, j, 100, 50, 16, 68.6, 100, 15)
 }
-for (j=1030; j<1200; j+=200) {
-  rectangle(900, j, 100, 50, 16, 68.6, 100, -15)
+for (j=1230; j<1400; j+=200) {
+  rectangle(850, j, 100, 50, 16, 68.6, 100, -15)
 }
 
-for (j=450; j<700; j+=200) {
+for (j=600; j<850; j+=200) {
   rectangle(700, j, 100, 50, 16, 68.6, 100, 15)
 }
-for (j=1050; j<1400; j+=200) {
-  rectangle(700, j, 100, 50, 16, 68.6, 100, -15)
+for (j=1250; j<1600; j+=200) {
+  rectangle(650, j, 100, 50, 16, 68.6, 100, -15)
 }
 
-for (j=230; j<800; j+=200) {
+for (j=380; j<950; j+=200) {
   rectangle(500, j, 100, 50, 16, 68.6, 100, 15)
 }
-for (j=1070; j<1600; j+=200) {
-  rectangle(500, j, 100, 50, 16, 68.6, 100, -15)
+for (j=1270; j<1800; j+=200) {
+  rectangle(450, j, 100, 50, 16, 68.6, 100, -15)
 }
 
-for (j=10; j<800; j+=200) {
+for (j=160; j<950; j+=200) {
   rectangle(300, j, 100, 50, 16, 68.6, 100, 15)
 }
-for (j=1090; j<1800; j+=200) {
-  rectangle(300, j, 100, 50, 16, 68.6, 100, -15)
+for (j=1290; j<2000; j+=200) {
+  rectangle(250, j, 100, 50, 16, 68.6, 100, -15)
 }
-for (j=-10; j<700; j+=200) {
+for (j=150; j<800; j+=200) {
   rectangle(100, j, 100, 50, 16, 68.6, 100, 15)
 }
-for (j=1110; j<1800; j+=200) {
-  rectangle(100, j, 100, 50, 16, 68.6, 100, -15)
+for (j=1300; j<2000; j+=200) {
+  rectangle(50, j, 100, 50, 16, 68.6, 100, -15)
 }
 
 
@@ -150,37 +156,37 @@ for (j=1110; j<1800; j+=200) {
 // }
 
 for (j=720; j<800; j+=200) {
-  rectangle(900, j, 100, 50, 5, 99.6, 99.8, 15)
+  rectangle(850, j, 100, 50, 5, 99.6, 99.8, 15)
 }
-for (j=1080; j<1200; j+=200) {
+for (j=1120; j<1250; j+=200) {
   rectangle(900, j, 100, 50, 5, 99.6, 99.8, -15)
 }
 
 for (j=500; j<800; j+=200) {
-  rectangle(700, j, 100, 50, 5, 99.6, 99.8, 15)
+  rectangle(650, j, 100, 50, 5, 99.6, 99.8, 15)
 }
-for (j=1100; j<1500; j+=200) {
+for (j=1150; j<1550; j+=200) {
   rectangle(700, j, 100, 50, 5, 99.6, 99.8, -15)
 }
 
 for (j=280; j<800; j+=200) {
-  rectangle(500, j, 100, 50, 5, 99.6, 99.8, 15)
+  rectangle(450, j, 100, 50, 5, 99.6, 99.8, 15)
 }
-for (j=1120; j<1700; j+=200) {
+for (j=1170; j<1750; j+=200) {
   rectangle(500, j, 100, 50, 5, 99.6, 99.8, -15)
 }
 
 for (j=60; j<800; j+=200) {
-  rectangle(300, j, 100, 50, 5, 99.6, 99.8, 15)
+  rectangle(250, j, 100, 50, 5, 99.6, 99.8, 15)
 }
-for (j=1140; j<1800; j+=200) {
+for (j=1190; j<1850; j+=200) {
   rectangle(300, j, 100, 50, 5, 99.6, 99.8, -15)
 }
 
 for (j=40; j<700; j+=200) {
-  rectangle(100, j, 100, 50, 5, 99.6, 99.8, 15)
+  rectangle(50, j, 100, 50, 5, 99.6, 99.8, 15)
 }
-for (j=1160; j<1800; j+=200) {
+for (j=1210; j<1850; j+=200) {
   rectangle(100, j, 100, 50, 5, 99.6, 99.8, -15)
 }
 
