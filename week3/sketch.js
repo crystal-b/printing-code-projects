6c945d66b2a16d1b800fe943de9c84da07898998
crystal-b <git@github.com:crystal-b>
2016-01-14 17:08:42 -0500

    original sketch

--- week3/sketch.js
+++ week3/sketch.js
@@ -2,9 +2,9 @@
 
 var r = new Rune({
   container: "#canvas",
-  width: 850,
-  height: 1200,
-  debug: true
+  width: 1200,
+  height: 850,
+  debug: false
 });
 
 // var p = r.polygon(100,200)
@@ -15,6 +15,8 @@ var r = new Rune({
   //sample
  //r.ellipse(400, 350, 500, 600);
 
+
+/* WAVVVVVES */
  var x1 = 246;
  var x2 = 475;
  var x3 = 704;
@@ -28,21 +30,132 @@ var r = new Rune({
  var ny2 = 1050;
  var ny3 = 1180;
 
-var baloon = r.ellipse(475, 625, 500, 500)
-  .stroke(false)
-  .fill(0);
 
-var baloonTop = r.triangle(x1, y1, x2, y2, x3, y3)
-  .stroke(false)
-  .fill(0, 0, 0);
+ var height = 1300;
+ var width = 850;
+ var yStart = 0;
+ var y2 = 100;
 
-// var needle = r.triangle(nx1, ny1, nx2, ny2, nx3, ny3)
-// var needle = r.triangle(50, 1150, 200, 1050, 70, 1100)
-var needle = r.triangle(0, 1000, 200, 200, 100, 1000)
+function curvedUp() {
+    var wave = r.path(width/2, yStart)
+        .moveTo(0, 100)
+        .curveTo(100, 50)
+        //.moveTo(0, 500)
+        //.curveTo(900, 250)
+        .fill('hsv', 250, 100, 100, 1)
+        .stroke(false)
+    .rotate(0);
+}
+function curvedDown() {
+    var wave = r.path(width/2, y2)
+        .moveTo(0, 50)
+        .curveTo(-50, 25)
+        //.moveTo(0, 500)
+        //.curveTo(900, 250)
+        .fill('hsv', 250, 100, 100, 1)
+        .stroke(false)
+    .rotate(0);
+}
+function wet() {
+  curvedUp();
+  curvedDown();
+}
+
+wet();
+
+
+
+for (var i = 0; i <= height; i+=150) {
+  yStart = i;
+  wet();
+  y2 += 150;
+}
+
+
+/* CRYSTALS */
+
+
+
+var moveX = 100;
+var moveY = 200;
+var leftFacetX = 75;
+var leftFacetY = 0;
+var facetX1 = 0;
+var facetY1 = 400;
+var facetX2 = 100;
+var facetY2 = 450;
+var facetX3 = 150;
+var facetY3 = 550;
+var facetX4 = 300;
+var facetY4 = 400;
+var rightFacetX = 225;
+var rightFacetY = 0;
+var degree = 0;
+
+function droplet () {
+  var wet = r.path()
+  .fill(0, 0, 255)
   .stroke(false)
-  .fill(0, 255, 0)
-  .rotate(0)
-  console.log(needle.vars);
+  .degree(rotate);
+}
+
+function crystal (moveX, moveY, leftFacetX, leftFacetY,  facetX1, facetY1, facetX2, facetY2, facetX3, facetY3, facetX4, facetY4, rightFacetX, rightFacetY, degree) {
+  var sharp = r.polygon(moveX, moveY)
+    .lineTo(leftFacetX, leftFacetY)
+    .lineTo(facetX1, facetY1)
+    .lineTo(facetX2, facetY2)
+    .lineTo(facetX3, facetY3)
+    .lineTo(facetX4, facetY4)
+    .lineTo(rightFacetX, rightFacetY)
+    .fill(0, 255, 0)
+    .stroke(false)
+    .rotate(degree);
+}
+
+crystal(100, 200, 75, 0, 0, 400, 100, 450, 150, 550, 300, 400, 225, 0, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// r.path(xStart, yStart)
+//   .moveTo(xEnd, yEnd)
+//   .curveTo(xMidpoint, yMidpoint)
+
+
+
+// var balloonTop = r.triangle(x1, y1, x2, y2, x3, y3)
+//   .stroke(false)
+//   .fill(0, 0, 0);
+
+// // var needle = r.triangle(nx1, ny1, nx2, ny2, nx3, ny3)
+// // var needle = r.triangle(50, 1150, 200, 1050, 70, 1100)
+// var needle = r.triangle(0, 1000, 200, 200, 100, 1000)
+//   .stroke(false)
+//   .fill(0, 255, 0)
+//   .rotate(0)
+//   console.log(needle.vars);
 
 // var b = r.polygon(0,0)
 //   .lineTo(700, 325)
@@ -73,6 +186,6 @@ var needle = r.triangle(0, 1000, 200, 200, 100, 1000)
 //   }
 
 
-
+//balloon();
 
 r.draw();
